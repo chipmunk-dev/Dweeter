739fd04a59ecdda675520f12a4c55ff0976db32b
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-19 22:53:36 +0900

    fix: update tweet controller

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 
-import tweetsRoute from "./router/tweetRouter.js";
+import tweetsRouter from "./router/tweetRouter.js";
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("tiny"));
 
-app.use("/tweets", tweetsRoute);
+app.use("/tweets", tweetsRouter);
 
 app.use((req, res, next) => {
 	res.sendStatus(404);
--- router/tweetRouter.js
+++ router/tweetRouter.js
@@ -3,77 +3,56 @@ import express from "express";
 const router = express.Router();
 
 let incereaseTweetId = 3;
-const tweets = [
+let tweets = [
 	{
 		id: 1,
 		text: "안녕하세요?",
-		createdAt: new Date().getDate(),
+		createdAt: Date.now().toString(),
 		name: "quokka",
 		userName: "quokka54",
 	},
 	{
 		id: 2,
 		text: "친구 구합니다.",
-		createdAt: new Date().getDate(),
+		createdAt: Date.now().toString(),
 		name: "chipmunk",
 		userName: "dev_chip",
 	},
 ];
 
-// 전체 조회
+// 1. 전체 조회
+// 2. 유저 트윗 조회
 router.get("/", (req, res, next) => {
-	res.status(200).json(tweets);
-});
-
-// 유저 트윗 조회
-router.get("/user", (req, res, next) => {
 	const { userName } = req.query;
+	const data = userName ? tweets.filter(t => t.userName === userName) : tweets;
 
-	if (!userName) {
-		return res.status(400).send(`"userName" is missing`);
-	}
-
-	const tweetList = tweets.filter(tweet => tweet.userName === userName);
-
-	if (tweetList.length) {
-		return res.status(200).json(tweetList);
-	} else {
-		return res.status(400).send(`"${userName}" is not exist`);
-	}
+	res.status(200).json(data);
 });
 
 // 단일 조회
 router.get("/:id", (req, res, next) => {
 	const { id } = req.params;
-
-	if (!id) return res.status(400).send(`"id" is missing`);
-
 	const tweet = tweets.find(tweet => tweet.id === parseInt(id));
-
 	if (tweet) {
 		return res.status(200).json(tweet);
 	} else {
-		return res.status(400).send(`"${id}" is not exist`);
+		return res.status(404).json({ message: `Tweet ${id} is not found` });
 	}
 });
 
 // 생성
 router.post("/", (req, res, next) => {
 	const { text, name, userName, url } = req.body;
-
 	if (text && name && userName) {
 		const tweet = {
-			id: incereaseTweetId,
+			id: incereaseTweetId++,
 			text,
-			createdAt: new Date().getDate(),
+			createdAt: new Date(),
 			name,
 			userName,
 			url: url || null,
 		};
-
-		tweets.push(tweet);
-		incereaseTweetId++;
-
+		tweets = [tweet, ...tweets];
 		return res.sendStatus(201);
 	}
 
@@ -84,43 +63,22 @@ router.post("/", (req, res, next) => {
 router.put("/:id", (req, res, next) => {
 	const { id } = req.params;
 	const { text } = req.body;
+	const tweet = tweets.find(t => t.id === parseInt(id));
 
-	if (!id || !text) {
-		return res.status(400).send(`missing "id" or "text" is empty data `);
-	}
-
-	let isFind = false;
-
-	tweets.forEach(tweet => {
-		if (tweet.id === parseInt(id)) {
-			tweet.text = text;
-			isFind = true;
-		}
-	});
-
-	if (isFind) {
-		return res.sendStatus(201);
+	if (tweet) {
+		tweet.text = text;
+		return res.status(200).json(tweet);
 	} else {
-		return res.status(400).send(`${id} is not exist`);
+		return res.status(404).json({ message: `Tweet ${id} not found` });
 	}
 });
 
 // 삭제
 router.delete("/:id", (req, res, next) => {
 	const { id } = req.params;
-
-	if (!id) {
-		return res.status(400).send("missing id");
-	}
-
-	const findIndex = tweets.findIndex(tweet => tweet.id === parseInt(id));
-
-	if (findIndex > -1) {
-		tweets.splice(findIndex, 1);
-		return res.sendStatus(204);
-	} else {
-		return res.status(400).send(`${id} is not exist`);
-	}
+	if (!id) return res.status(400).send("missing id");
+	tweets = tweets.filter(t => t.id !== parseInt(id));
+	return res.sendStatus(204);
 });
 
 export default router;
