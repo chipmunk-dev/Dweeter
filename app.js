d0da7c83f00d1518c2d37bfe9d66032859d4422f
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-19 22:33:53 +0900

    feat: apply cors, helmet

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 
-import tweetRouter from "./router/tweetRouter.js";
+import tweetsRoute from "./router/tweetRouter.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan("short"));
+app.use(helmet());
+app.use(cors());
+app.use(morgan("tiny"));
 
-app.use("/tweets", tweetRouter);
+app.use("/tweets", tweetsRoute);
 
+app.use((req, res, next) => {
+	res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+	console.error(error);
+	res.sendStatus(500);
+});
 app.listen(8080, () => console.log("Connected..."));
