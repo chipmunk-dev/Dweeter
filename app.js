ce927b74189e1305f6c708602e69af6e04dbadd2
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-19 19:52:50 +0900

    feat: create tweet controller

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
+
+import tweetRouter from "./router/tweetRouter.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("short"));
 
 app.use("/tweets", tweetRouter);
 
--- router/tweetRouter.js
+++ router/tweetRouter.js
@@ -2,20 +2,125 @@ import express from "express";
 
 const router = express.Router();
 
-// 전체 조회
-router.get("/", (req, res, next) => {});
+let incereaseTweetId = 3;
+const tweets = [
+	{
+		id: 1,
+		text: "안녕하세요?",
+		createdAt: new Date().getDate(),
+		name: "quokka",
+		userName: "quokka54",
+	},
+	{
+		id: 2,
+		text: "친구 구합니다.",
+		createdAt: new Date().getDate(),
+		name: "chipmunk",
+		userName: "dev_chip",
+	},
+];
 
-// 단일 조회
-router.get("/:id", (req, res, next) => {});
+// 전체 조회
+router.get("/", (req, res, next) => {
+	res.status(200).json(tweets);
+});
 
 // 유저 트윗 조회
-router.get("/my", (req, res, next) => {});
+router.get("/user", (req, res, next) => {
+	const { userName } = req.query;
+
+	if (!userName) {
+		return res.status(400).send(`"userName" is missing`);
+	}
+
+	const tweetList = tweets.filter(tweet => tweet.userName === userName);
+
+	if (tweetList.length) {
+		return res.status(200).json(tweetList);
+	} else {
+		return res.status(400).send(`"${userName}" is not exist`);
+	}
+});
+
+// 단일 조회
+router.get("/:id", (req, res, next) => {
+	const { id } = req.params;
+
+	if (!id) return res.status(400).send(`"id" is missing`);
+
+	const tweet = tweets.find(tweet => tweet.id === parseInt(id));
+
+	if (tweet) {
+		return res.status(200).json(tweet);
+	} else {
+		return res.status(400).send(`"${id}" is not exist`);
+	}
+});
 
 // 생성
-router.post("/", (req, res, next) => {});
+router.post("/", (req, res, next) => {
+	const { text, name, userName, url } = req.body;
+
+	if (text && name && userName) {
+		const tweet = {
+			id: incereaseTweetId,
+			text,
+			createdAt: new Date().getDate(),
+			name,
+			userName,
+			url: url || null,
+		};
+
+		tweets.push(tweet);
+		incereaseTweetId++;
+
+		return res.sendStatus(201);
+	}
+
+	return res.status(400).send("text or name or userName is missing");
+});
 
 // 수정
-router.put("/", (req, res, next) => {});
+router.put("/:id", (req, res, next) => {
+	const { id } = req.params;
+	const { text } = req.body;
+
+	if (!id || !text) {
+		return res.status(400).send(`missing "id" or "text" is empty data `);
+	}
+
+	let isFind = false;
+
+	tweets.forEach(tweet => {
+		if (tweet.id === parseInt(id)) {
+			tweet.text = text;
+			isFind = true;
+		}
+	});
+
+	if (isFind) {
+		return res.sendStatus(201);
+	} else {
+		return res.status(400).send(`${id} is not exist`);
+	}
+});
 
 // 삭제
-router.put("/:id", (req, res, next) => {});
+router.delete("/:id", (req, res, next) => {
+	const { id } = req.params;
+
+	if (!id) {
+		return res.status(400).send("missing id");
+	}
+
+	const findIndex = tweets.findIndex(tweet => tweet.id === parseInt(id));
+
+	if (findIndex > -1) {
+		tweets.splice(findIndex, 1);
+		return res.sendStatus(204);
+	} else {
+		return res.status(400).send(`${id} is not exist`);
+	}
+});
+
+export default router;
