414ef7224eededaa030b09c19b2f4b85ccc940d9
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-22 23:00:13 +0900

    feat: create auth repository function

--- data/auth.js
+++ data/auth.js
@@ -1,11 +1,27 @@
+const members = [
+	{
+		id: 1,
+		userName: "admin",
+		password: "1234",
+		email: "admin@a.a",
+		name: "admin",
+		url: null,
+	},
+];
+
 export async function create(userName, password, email, name, url) {
-	return null;
+	const user = { userName, password, email, name, url };
+	members.push(user);
 }
 
 export async function login(userName, password) {
-	return null;
+	const user = members.find(
+		member => member.userName === userName && member.password === password
+	);
+
+	return user;
 }
 
 export async function me(token) {
-	return null;
+	// TODO: token 확인 후 user 정보 리턴
 }
