a1944385e2530ece42b770014036738bfd7ac8e4
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-23 13:54:33 +0900

    feat: tweet auth 인증 추가

--- router/tweetRouter.js
+++ router/tweetRouter.js
@@ -2,23 +2,27 @@ import express from "express";
 import { body } from "express-validator";
 
 import * as tweetController from "../controller/tweet.js";
+import { isAuth } from "../middleware/auth.js";
 import { validate } from "../middleware/validator.js";
 
 const router = express.Router();
 
 const validateTweet = [
-	body("text").trim().isLength({ min: 3 }).withMessage("text should be at least 3 characters"),
+	body("text")
+		.trim()
+		.isLength({ min: 3 })
+		.withMessage("text should be at least 3 characters"),
 	validate,
 ];
 
 router.get("/", tweetController.getTweets);
 
-router.get("/:id", tweetController.getTweet);
+router.get("/:id", isAuth, tweetController.getTweet);
 
-router.post("/", validateTweet, tweetController.createTweet);
+router.post("/", validateTweet, isAuth, tweetController.createTweet);
 
-router.put("/:id", validateTweet, tweetController.updateTweet);
+router.put("/:id", validateTweet, isAuth, tweetController.updateTweet);
 
-router.delete("/:id", tweetController.deleteTweet);
+router.delete("/:id", isAuth, tweetController.deleteTweet);
 
 export default router;
