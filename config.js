2f44aad8aa498acf12320f91d4f9622f74e1c580
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-23 23:42:35 +0900

    feat: 환경변수의 값이 없을경우 핸들링

--- config.js
+++ config.js
@@ -1,12 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+function required(key, defaultValue = undefined) {
+	const value = process.env[key] || defaultValue;
+
+	if (value == null) {
+		throw new Error(`Key ${key} is undefined`);
+	}
+
+	return value;
+}
+
 export const config = {
 	jwt: {
-		secretKey: process.env.JWT_SECRET,
-		expiresInSec: process.env.JWT_EXPIRES_SEC,
+		secretKey: required("JWT_SECRET"),
+		expiresInSec: required("JWT_EXPIRES_SEC", 87400),
 	},
 	bcrypt: {
-		saltRounds: process.env.BCRYPT_SALT_ROUNDS,
+		saltRounds: required("BCRYPT_SALT_ROUNDS", 10),
 	},
 };
