f969702ca9d68987c828f8548ac7c4c9971217fb
chipmunk-dev <devquokkajeong@gmail.com>
2023-02-24 11:40:23 +0900

    fix: fix to import statement error

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ import helmet from "helmet";
 import tweetsRouter from "./router/tweetRouter.js";
 import authRouter from "./router/auth.js";
 import { config } from "./config.js";
-import { Server } from "socket.io";
+import { initSocket } from "./connection/socket.js";
+import { db } from "./data/db/database.js";
 
 const app = express();
 
@@ -26,5 +27,7 @@ app.use((error, req, res, next) => {
 	console.error(error);
 	res.sendStatus(500);
 });
+
+db.getConnection().then(connection => console.log(connection));
 const server = app.listen(config.host.port, () => console.log("Connected..."));
 initSocket(server);
--- data/db/database.js
+++ data/db/database.js
@@ -1,9 +1,11 @@
 import { createPool } from "mysql2";
-import { config } from "../../config";
+import { config } from "../../config.js";
 
-export const pool = createPool({
+const pool = createPool({
 	host: config.db.host,
 	user: config.db.user,
 	database: config.db.database,
 	password: config.db.password,
 });
+
+export const db = pool.promise();
